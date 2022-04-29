2bb867761a8803bca228bee667f11471f7e33463
Maxim Polyakov <maxpol1997@gmail.com>
2022-04-30 01:14:47 +0400

    try

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-const history = require('connect-history-api-fallback');
+const express = require('express')
+const path = require('path')
+const history = require('connect-history-api-fallback')
+// ^ middleware to redirect all URLs to index.html
 
-const app = express();
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname))
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(staticFileMiddleware)
+app.use(history())
+app.use(staticFileMiddleware)
+// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
 
-app.get('/api', (req, res) => res.send('Hello World!'));
-app.use(history({
-  verbose: true
-}));
-app.use('/', express.static(path.join(__dirname, 'dist')));
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/index.html'))
+})
 
-var port = process.env.PORT || 8080;
-app.listen(port);
-console.log('server started '+ port);
+app.listen(5000, function () {
+  console.log( 'Express serving on 5000!' )
+})
