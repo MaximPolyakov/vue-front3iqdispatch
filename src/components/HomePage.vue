a806bee97f0e4524a58cfb42a87fa63af90b0be0
Maxim Polyakov <maxpol1997@gmail.com>
2023-11-14 04:00:07 +0400

    changes for DAT demo small fix v.2

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -8,7 +8,7 @@
           <div class="col">
             <div class="cols1">
               <div class="c1_title">
-                <h1>Dispatcher <span class="red">Load Analytics </span> Tool
+                <h1>Dispatcher <span class="red">Load Analytics </span> Tools
                 </h1>
                 <h1>
                   Designed to increase load profitability <br> and to improve dispatcher productivity
